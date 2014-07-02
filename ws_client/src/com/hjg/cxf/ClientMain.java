c730f97f4c0119f139b4fdee8067245cdaa1d789
zhaofeng555 <443842006@qq.com>
2014-07-02 22:18:07 +0800

    webservice 客户端

---
+++ ws_client/src/com/hjg/cxf/ClientMain.java
@@ -0,0 +1,36 @@
+package com.hjg.cxf;
+
+import java.util.List;
+
+import com.hjg.cxf.ws.Cat;
+import com.hjg.cxf.ws.Entry;
+import com.hjg.cxf.ws.HelloWorld;
+import com.hjg.cxf.ws.StringCat;
+import com.hjg.cxf.ws.User;
+import com.hjg.cxf.ws.impl.HelloWorldWs;
+
+public class ClientMain {
+
+	public static void main(String[] args) {
+		HelloWorldWs factory = new HelloWorldWs();
+		HelloWorld hw = factory.getHelloWorldWsPort();
+		System.out.println(hw.sayHi("风哥"));
+		
+		User u = new User();
+		u.setId(1);
+		u.setUsername("sun");
+		u.setPassword("1122");
+		u.setAddress("家");
+		List<Cat> cats = hw.getCatByUser(u);
+		for (Cat c : cats) {
+			System.out.println(c.getName()+",  "+c.getColor());
+		}
+		
+		StringCat sc = hw.getCats();
+		List<Entry> ens = sc.getEntries();
+		for (Entry en : ens) {
+			System.out.println(en.getKey()+" "+en.getValue());
+		}
+	}
+
+}
---
+++ ws_client/src/com/hjg/cxf/ws/Cat.java
@@ -0,0 +1,113 @@
+
+package com.hjg.cxf.ws;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>cat complex type�� Java �ࡣ
+ * 
+ * <p>����ģʽƬ��ָ�����ڴ����е�Ԥ�����ݡ�
+ * 
+ * <pre>
+ * &lt;complexType name="cat">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="color" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="id" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *         &lt;element name="name" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "cat", propOrder = {
+    "color",
+    "id",
+    "name"
+})
+public class Cat {
+
+    protected String color;
+    protected int id;
+    protected String name;
+
+    /**
+     * ��ȡcolor���Ե�ֵ��
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * ����color���Ե�ֵ��
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setColor(String value) {
+        this.color = value;
+    }
+
+    /**
+     * ��ȡid���Ե�ֵ��
+     * 
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * ����id���Ե�ֵ��
+     * 
+     */
+    public void setId(int value) {
+        this.id = value;
+    }
+
+    /**
+     * ��ȡname���Ե�ֵ��
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * ����name���Ե�ֵ��
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+	@Override
+	public String toString() {
+		return "Cat [color=" + color + ", id=" + id + ", name=" + name + "]";
+	}
+    
+    
+
+}
