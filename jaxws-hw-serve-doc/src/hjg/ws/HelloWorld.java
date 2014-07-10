7b9568e6e2453ea054129a965af82779c738fc96
haojg <443842006@qq.com>
2014-07-10 11:32:03 +0800

    jaxws服务器端-document style形式

---
+++ jaxws-hw-serve-doc/src/hjg/ws/HelloWorld.java
@@ -0,0 +1,16 @@
+package hjg.ws;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.Use;
+
+@WebService
+@SOAPBinding(style = Style.DOCUMENT, use=Use.LITERAL) //optional
+public interface HelloWorld {
+
+	@WebMethod
+	String getHelloWorldAsString(String name);
+	
+}
---
+++ jaxws-hw-serve-doc/src/hjg/ws/HelloWorldPublisher.java
@@ -0,0 +1,12 @@
+package hjg.ws;
+
+import javax.xml.ws.Endpoint;
+
+public class HelloWorldPublisher {
+
+	public static void main(String[] args) {
+		Endpoint.publish("http://localhost:9999/ws/hello", new HelloWorldImpl());
+		System.out.println("jaxws docment style started ............... ");
+	}
+
+}
